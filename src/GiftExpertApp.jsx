1856e127c82bebd903f48e0864757191121caff5
Jhonny <jhonnycgarcia@gmail.com>
2024-11-04 16:23:46 -0500

    Crear una lista de categorias

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react"
+
 export const GiftExpertApp = () => {
+
+  const [categories, setcategories] = useState(['One Punch', 'Dragon Ball'])
+
   return (
     <>
         {/* Titulo */}
@@ -7,6 +12,11 @@ export const GiftExpertApp = () => {
         {/* Input */}
 
         {/* Listado de Gifts */}
+        <ol>
+          { categories.map((category, index) => {
+            return (<li key={index}>{category}</li>)
+          })}
+        </ol>
           {/* Gift item */}
     </>
   )
