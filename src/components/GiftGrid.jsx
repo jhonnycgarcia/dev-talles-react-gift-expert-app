cbdb815895d5eb164c18f1e7af6740abf430e27f
Jhonny <jhonnycgarcia@gmail.com>
2024-11-04 19:47:01 -0500

    Mostrar mensaje de carga

--- src/components/GiftGrid.jsx
+++ src/components/GiftGrid.jsx
@@ -1,29 +1,19 @@
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
 
 import { GiftItem } from './GiftItem';
 
-import { getGiftsByCategory } from '../helpers/getGiftsByCategory';
 import { useFetchGifs } from '../hooks/useFetchGifs';
-
-
 
 export const GiftGrid = ({ category }) => {
 
     const { images, isLoading} = useFetchGifs(category);
-    // const [images, setimages] = useState([]);
-
-
-    // useEffect(() => {
-    //     getImages(images);
-    // //   return () => {
-    // //     second
-    // //   }
-    // }, [ ]);
 
     return (
         <>
             <h3>{category}</h3>
+
+            { isLoading && <h2>Cargando...</h2> }
+
             <div className='card-grid'>
                 { images.map((image) => (
                     <GiftItem 
